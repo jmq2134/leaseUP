e4397db9d8c25ea8026e77b1203429d190eade5a
Julie Quinn <Julie@first-interstate.local>
2017-09-05 16:17:45 -0400

    changes to dashboard navbar and added route outline

---
+++ routes/api-routes.js
@@ -0,0 +1,50 @@
+// Routes =============================================================
+var db = require("../models/index.js");
+
+// ---------------------------- GET ROUTES ---------------------------- //
+
+    /// SHOW LOGIN ON LOAD
+    app.get("/", function(req, res) {
+        res.render('login.html', req);
+    });
+
+    /// REDIRECT FROM LOGIN TO DASHBOARD IF VALIDATED
+
+
+    /// ROUTE TO AVON
+
+
+    /// ROUTE TO HAWTHORNE
+
+
+    /// ROUTE TO LEGACY
+
+
+    /// ROUTE TO MENTOR
+
+
+    /// ROUTE TO STEELYARD
+
+
+    /// ROUTE TO OAKWOOD
+
+
+    /// SHOW ALL TENANT INFO
+
+
+    /// SHOW ALL SHOPPING CENTER INFO
+
+
+// ---------------------------- POST ROUTES ---------------------------- //
+
+	/// ADD A NEW TENANT
+
+
+
+
+	/// EDIT AN EXISTING TENANT
+
+
+
+
+	/// DELETE A TENANT
