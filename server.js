feaf0fdc806e77c2e69545120e0279b36c54ca29
Julie Quinn <jmq2134@gmail.com>
2017-10-06 16:58:21 -0400

    remove force true

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ var authRoute = require('./routes/auth.js')(app, passport);
 
 
 // Syncing sequelize models and then starting the express app
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("App listening on PORT " + PORT);
     });
